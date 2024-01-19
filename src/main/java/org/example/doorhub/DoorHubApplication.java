6955a7655701cd542ccfa00c4c54763e763da319
egoist <rikhsiboyevzaxa@gmail.com>
2024-01-19 22:33:00 +0500

    first commit

---
+++ src/main/java/org/example/doorhub/DoorHubApplication.java
@@ -0,0 +1,13 @@
+package org.example.doorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/doorhub/DoorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
