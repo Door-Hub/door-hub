128509717e3d904eb32a4cd04f1efa76592cef7e
Sayyod <136818768+Sayyod571@users.noreply.github.com>
2024-01-24 15:48:52 +0500

    delete-view

---
+++ src/main/java/org/example/doorhub/review/ReviewController.java
@@ -0,0 +1,16 @@
+package org.example.doorhub.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+
+}
---
+++ src/main/java/org/example/doorhub/review/ReviewService.java
@@ -0,0 +1,9 @@
+package org.example.doorhub.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+}
