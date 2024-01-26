0c720dc9697c2ce7079e0451b7c23fa74fa92077
Sayyod <136818768+Sayyod571@users.noreply.github.com>
2024-01-26 18:26:11 +0500

    create

--- src/main/java/org/example/doorhub/attachment/entity/Attachment.java
+++ src/main/java/org/example/doorhub/attachment/entity/Attachment.java
@@ -4,23 +4,34 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.example.doorhub.user.entity.User;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name = "attachment")
 public class Attachment {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String fileName;
-    private String OriginalFileName;
-    private String url;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String file_name;
+
+    @Column(nullable = false)
     private String fileType;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    private String url;
+
+    @Column(nullable = false)
+    private LocalDateTime uploadTime;
+
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+
 }
